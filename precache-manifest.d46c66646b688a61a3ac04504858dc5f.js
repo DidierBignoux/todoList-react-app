69074c88ee4ec53251eb9636ed43f08167e011e2
DidierBignoux <didieransleybignoux@live.com>
2019-07-20 12:10:43 +0400

    Updates

---
+++ precache-manifest.d46c66646b688a61a3ac04504858dc5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30697dd8309e9cb525aab0987a403648",
+    "url": "/todoList-react-app/index.html"
+  },
+  {
+    "revision": "38258227954afaffe456",
+    "url": "/todoList-react-app/static/css/main.18b51434.chunk.css"
+  },
+  {
+    "revision": "08e72e7f05dd5b0c7721",
+    "url": "/todoList-react-app/static/js/2.adf86020.chunk.js"
+  },
+  {
+    "revision": "38258227954afaffe456",
+    "url": "/todoList-react-app/static/js/main.8a766f4c.chunk.js"
+  },
+  {
+    "revision": "c688aba92dd29dc70859",
+    "url": "/todoList-react-app/static/js/runtime~main.774eebdf.js"
+  }
+]);
